bfff10f0f6c0010a7df8cf6b551927891e912a64
Alexander.Chukov@gmail.com <Alexander.Chukov@gmail.com@851b78de-e7be-efcc-6916-3c5f4dcf81d5>
2013-08-24 06:06:31 +0000

    uncommented temporary disabled builds

--- trunk/cctools-repo/build.sh
+++ trunk/cctools-repo/build.sh
@@ -14,13 +14,13 @@ done
 
 export PATH=${WRKDIR}/arm-android/bin:${WRKDIR}/mips-android/bin:${WRKDIR}/x86-android/bin:/home/opt/CodeSourcery/Sourcery_G++_Lite/bin:/opt/CodeSourcery/Sourcery_G++_Lite/bin:$PATH
 
-#${NDKDIR}/build/tools/make-standalone-toolchain.sh --arch=arm --install-dir=${WRKDIR}/arm-android --toolchain=arm-linux-androideabi-4.8
-#./build-shell-utils.sh ${PWD}/src arm-linux-androideabi ${WRKDIR}/arm-repo ${WRKDIR}/arm-android/sysroot || exit 1
+${NDKDIR}/build/tools/make-standalone-toolchain.sh --arch=arm --install-dir=${WRKDIR}/arm-android --toolchain=arm-linux-androideabi-4.8
+./build-shell-utils.sh ${PWD}/src arm-linux-androideabi ${WRKDIR}/arm-repo ${WRKDIR}/arm-android/sysroot || exit 1
 
-#${NDKDIR}/build/tools/make-standalone-toolchain.sh --arch=mips --install-dir=${WRKDIR}/mips-android --toolchain=mipsel-linux-android-4.8
-#./build-shell-utils.sh ${PWD}/src mipsel-linux-android  ${WRKDIR}/mips-repo ${WRKDIR}/mips-android/sysroot || exit 1
+${NDKDIR}/build/tools/make-standalone-toolchain.sh --arch=mips --install-dir=${WRKDIR}/mips-android --toolchain=mipsel-linux-android-4.8
+./build-shell-utils.sh ${PWD}/src mipsel-linux-android  ${WRKDIR}/mips-repo ${WRKDIR}/mips-android/sysroot || exit 1
 
-#${NDKDIR}/build/tools/make-standalone-toolchain.sh --arch=x86 --install-dir=${WRKDIR}/x86-android --toolchain=x86-4.8
+${NDKDIR}/build/tools/make-standalone-toolchain.sh --arch=x86 --install-dir=${WRKDIR}/x86-android --toolchain=x86-4.8
 ./build-shell-utils.sh ${PWD}/src i686-linux-android    ${WRKDIR}/i686-repo ${WRKDIR}/x86-android/sysroot || exit 1
 
 echo "DONE!"
