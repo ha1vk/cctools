c66a248b76719b43dd15f731646da38983c3a21c
Alexander.Chukov@gmail.com <Alexander.Chukov@gmail.com@851b78de-e7be-efcc-6916-3c5f4dcf81d5>
2013-10-24 12:59:23 +0000

    fix

--- trunk/cctools-repo/build.sh
+++ trunk/cctools-repo/build.sh
@@ -12,7 +12,7 @@ for f in ../cctools-native/patches/*; do
     ln -sf ../${f} patches/
 done
 
-export PATH=${WRKDIR}/arm-android/bin:${WRKDIR}/mips-android/bin:${WRKDIR}/x86-android/bin:/home/opt/CodeSourcery/Sourcery_G++_Lite/bin:/opt/CodeSourcery/Sourcery_G++_Lite/bin:$PATH
+export PATH=${WRKDIR}/arm-android/bin:${WRKDIR}/mips-android/bin:${WRKDIR}/x86-android/bin:/opt/CodeSourcery/bin:$PATH
 
 ${NDKDIR}/build/tools/make-standalone-toolchain.sh --arch=arm --install-dir=${WRKDIR}/arm-android --toolchain=arm-linux-androideabi-4.8
 ./build-shell-utils.sh ${PWD}/src arm-linux-androideabi ${WRKDIR}/arm-repo ${WRKDIR}/arm-android/sysroot || exit 1
