b2ffe360e4eee4b42c47d63f8aeec72b55848832
Alexander.Chukov@gmail.com <Alexander.Chukov@gmail.com@851b78de-e7be-efcc-6916-3c5f4dcf81d5>
2013-11-01 05:26:43 +0000

    meta packages update

--- trunk/cctools-repo/rules/build-essential-gcc-objc-fortran.sh
+++ trunk/cctools-repo/rules/build-essential-gcc-objc-fortran.sh
@@ -1,11 +1,11 @@
-build_build_essential_clang() {
-    PKG=build-essential-clang
+build_build_essential_gcc_objc_fortran() {
+    PKG=build-essential-gcc-objc-fortran
     PKG_VERSION=1.0
-    PKG_DESC="Informational list of clang build-essential packages"
-    PKG_DEPS="busybox binutils clang libgcc-dev cxxstl-dev make ndk-misc ndk-sysroot-\${HOSTNDKARCH}-\${HOSTNDKVERSION} cctools-examples"
+    PKG_DESC="Informational list of gcc,objc,fortran build-essential packages"
+    PKG_DEPS="busybox binutils gobjc gfortran make ndk-misc ndk-sysroot-\${HOSTNDKARCH}-\${HOSTNDKVERSION} cctools-examples gfortran-examples"
     c_tag ${PKG} && return
 
-    banner "Build clang build essential"
+    banner "Build gcc objc fortran build essential"
 
     mkdir -p ${TMPINST_DIR}/${PKG}/cctools
 
