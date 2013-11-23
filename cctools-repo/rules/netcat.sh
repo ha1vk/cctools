cc6c9756048a779d2adb3f33b0b421ff6451e241
Alexander.Chukov@gmail.com <Alexander.Chukov@gmail.com@851b78de-e7be-efcc-6916-3c5f4dcf81d5>
2013-11-23 18:52:12 +0000

    move builtin sources to misc directory

--- cctools-repo/rules/netcat.sh
+++ cctools-repo/rules/netcat.sh
@@ -8,8 +8,8 @@ build_netcat() {
 
     mkdir -p ${TMPINST_DIR}/${PKG}/cctools/bin
 
-    ${TARGET_ARCH}-gcc ${SRC_PREFIX}/nc/netcat.c -s -o ${TMPINST_DIR}/${PKG}/cctools/bin/netcat || error "compilation"
-    ln -s netcat ${TMPINST_DIR}/${PKG}/cctools/bin/nc
+    ${TARGET_ARCH}-gcc ${TOPDIR}/misc/nc/netcat.c -s -o ${TMPINST_DIR}/${PKG}/cctools/bin/netcat || error "compilation"
+    ln -sf netcat ${TMPINST_DIR}/${PKG}/cctools/bin/nc
 
     local filename="${PKG}_${PKG_VERSION}_${PKG_ARCH}.zip"
     build_package_desc ${TMPINST_DIR}/${PKG} $filename ${PKG} $PKG_VERSION $PKG_ARCH "$PKG_DESC"
