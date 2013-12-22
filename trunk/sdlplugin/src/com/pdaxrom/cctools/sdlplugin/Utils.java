02e24320ab611285177127b102141b99c1abc22b
Alexander.Chukov@gmail.com <Alexander.Chukov@gmail.com@851b78de-e7be-efcc-6916-3c5f4dcf81d5>
2013-12-22 15:31:43 +0000

    sdl plugin

---
+++ trunk/sdlplugin/src/com/pdaxrom/cctools/sdlplugin/Utils.java
@@ -0,0 +1,55 @@
+package com.pdaxrom.cctools.sdlplugin;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Utils {
+	public static void copyDirectory(File sourceLocation , File targetLocation) throws IOException {
+	    if (sourceLocation.isDirectory()) {
+	        if (!targetLocation.exists()) {
+	            targetLocation.mkdir();
+	        }
+
+	        String[] children = sourceLocation.list();
+	        for (int i=0; i<children.length; i++) {
+	            copyDirectory(new File(sourceLocation, children[i]),
+	                    new File(targetLocation, children[i]));
+	        }
+	    } else {
+	        InputStream in = new FileInputStream(sourceLocation);
+	        OutputStream out = new FileOutputStream(targetLocation);
+
+	        // Copy the bits from instream to outstream
+	        byte[] buf = new byte[1024*1024];
+	        int len;
+	        while ((len = in.read(buf)) > 0) {
+	            out.write(buf, 0, len);
+	        }
+	        in.close();
+	        out.close();
+	    }
+	}
+	
+	public static void deleteDirectory(File file) {
+		if (file.isDirectory()) {
+			if (file.list().length == 0) {
+				file.delete();
+			} else {
+				String files[] = file.list();
+				for (String temp: files) {
+					File fileDelete = new File(file, temp);
+					deleteDirectory(fileDelete);
+				}
+				if (file.list().length == 0) {
+					file.delete();
+				}
+			}
+		} else {
+			file.delete();
+		}
+	}
+}
