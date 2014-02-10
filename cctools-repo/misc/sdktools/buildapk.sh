f3c820803cf03cb9025b19d68b0fd9e3d2070d35
Alexander.Chukov@gmail.com <Alexander.Chukov@gmail.com@851b78de-e7be-efcc-6916-3c5f4dcf81d5>
2014-02-10 12:13:50 +0000

    updated

--- cctools-repo/misc/sdktools/buildapk.sh
+++ cctools-repo/misc/sdktools/buildapk.sh
@@ -10,9 +10,29 @@ if [ ! -f AndroidManifest.xml ]; then
     exit 1
 fi
 
+VERBOSE_JAVAC=
+VERBOSE_JAR=
+VERBOSE_DEX=
+VERBOSE_APKBUILDER=
+VERBOSE_AAPT=
+
+if [ "$1" = "-v" ]; then
+    VERBOSE_JAVAC="-verbose"
+    VERBOSE_JAR="v"
+    VERBOSE_DEX="--verbose"
+    VERBOSE_APKBUILDER="-v"
+    VERBOSE_AAPT="-v"
+    shift
+fi
+
 if [ "$1" = "clean" ]; then
     rm -rf build gen
     exit 0
+fi
+
+PROJECT_RELEASE="no"
+if [ "$1" = "release" ]; then
+    PROJECT_RELEASE="yes"
 fi
 
 TOPDIR=$1
@@ -23,13 +43,30 @@ fi
 
 TOPDIR=`realpath $TOPDIR`
 
-APKNAME=`basename $TOPDIR`
+PROJECTNAME=`aproject-helper AppName`
+if [ "$PROJECTNAME" = "" ]; then
+    PROJECTNAME=`basename $TOPDIR`
+fi
 
 mkdir -p build/classes gen
 
-ANDROID_SDK_VER=17
+ANDROID_SDK_VER=`aproject-helper TargetSDK`
+if [ "$ANDROID_SDK_VER" = "" ]; then
+    ANDROID_SDK_VER=`aproject-helper MinSDK`
+fi
+
+if [ "$ANDROID_SDK_VER" = "" ]; then
+    echo "WARNING: No SDK version found in AndroidManifest.xml!"
+    ANDROID_SDK_VER=`getprop ro.build.version.sdk`
+    echo "WARNING: Use OS defined version $ANDROID_SDK_VER"
+fi
 
 ANDROID_SDK=${CCTOOLSDIR}/sdk/android-${ANDROID_SDK_VER}
+
+if [ ! -d $ANDROID_SDK ]; then
+    echo "ERROR: No SDK Android-${ANDROID_SDK_VER} found!"
+    exit 1
+fi
 
 find_jar_for_aapt() {
     local d=
@@ -54,14 +91,26 @@ find_jar_for_javac() {
     done
 }
 
-aapt p -f -v -M AndroidManifest.xml -F ./build/resources.res `find_jar_for_aapt $ANDROID_SDK libs $ANDROID_LIBS` -S res/ -J gen || error "aapt"
+if [ "$PROJECT_RELEASE" = "yes" ]; then
+    aproject-helper BuildConfig release
+else
+    aproject-helper BuildConfig debug
+fi
+
+aapt p -f $VERBOSE_AAPT -M AndroidManifest.xml -F ./build/resources.res `find_jar_for_aapt $ANDROID_SDK libs $ANDROID_LIBS` -S res/ -J gen || error "aapt"
 
 find . -name "*.aidl" -exec aidl -Isrc `find_aidl_for_aidl $ANDROID_SDK $ANDROID_AIDL` -ogen {} \;
 
-javac -verbose `find_jar_for_javac $ANDROID_SDK libs $ANDROID_LIBS` -d build/classes `find src -name "*.java"` `find gen -name "*.java"` || error "javac"
+javac $VERBOSE_JAVAC `find_jar_for_javac $ANDROID_SDK libs $ANDROID_LIBS` -d build/classes `find src -name "*.java"` `find gen -name "*.java"` || error "javac"
 
-dx --dex --verbose --no-strict --output=build/${APKNAME}.dex `find build/classes -maxdepth 1 -not -path "build/classes"` || error "dx"
+ACTIVITIES=`aproject-helper Activities`
 
-apkbuilder build/${APKNAME}.apk -v -u -z ./build/resources.res -f ./build/${APKNAME}.dex || error "apkbuilder"
+if [ "$ACTIVITIES" = "" ]; then
+    jar c${VERBOSE_JAR}f build/${PROJECTNAME}.jar -C build/classes .
+else
+    dx --dex $VERBOSE_DEX --no-strict --output=build/${PROJECTNAME}.dex `find build/classes -maxdepth 1 -not -path "build/classes"` || error "dx"
 
-apksigner build/${APKNAME}.apk build/${APKNAME}-signed.apk || error "apksigner"
+    apkbuilder build/${PROJECTNAME}.apk $VERBOSE_APKBUILDER -u -z ./build/resources.res -f ./build/${PROJECTNAME}.dex || error "apkbuilder"
+
+    apksigner build/${PROJECTNAME}.apk build/${PROJECTNAME}-signed.apk || error "apksigner"
+fi
